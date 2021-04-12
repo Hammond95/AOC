ce6820811e80f7c16c2bd8284bb08a7124331f21
Hammond95 <mdeluca@cuebiq.com>
2021-04-12 23:35:35 +0200

    2020 Day1 in golang.

--- 2020/Day1/golang/day1.go
+++ 2020/Day1/golang/day1.go
@@ -1,12 +1,30 @@
 package main
 	
 import (
-    //"bufio"
+    "bufio"
     "fmt"
     //"io"
-    "io/ioutil"
-    //"os"
+    //"io/ioutil"
+    "os"
+    "strconv"
+    "sort"
 )
+
+func readLines(path string) ([]int, error) {
+    file, err := os.Open(path)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var lines []int
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        convertedLine, _ := strconv.Atoi(scanner.Text())
+        lines = append(lines, convertedLine)
+    }
+    return lines, scanner.Err()
+}
 
 func check(e error) {
     if e != nil {
@@ -14,8 +32,57 @@ func check(e error) {
     }
 }
 
+func getSumPair(elements []int, search int) (int, int) {
+    size := len(elements)
+    fmt.Printf("size = %d\n", size)
+    pairedPosFirst := -1
+    pairedPosSecond := -1
+    for i, element := range elements {
+        fmt.Printf("#%d %d\n", i, element)
+        paired := search - element
+        pivot := size/2
+        upperBound := size-1
+        lowerBound := i
+        stayIn := true
+        for (stayIn && (upperBound-lowerBound > 1)) {
+            if (elements[pivot] == paired){
+                stayIn = false
+                pairedPosFirst = i
+                pairedPosSecond = pivot
+            } else if (elements[pivot] < paired) {
+                lowerBound = pivot
+                pivot = (pivot + upperBound)/2
+            } else {
+                upperBound = pivot
+                pivot = (lowerBound + pivot)/2
+            }
+            fmt.Printf("pivot = %d, lb = %d, ub = %d\n", pivot, lowerBound, upperBound)
+        }
+        if (pairedPosSecond != -1){ break }
+        fmt.Printf("#%d %d\n", i, element)
+    }
+
+    if (pairedPosSecond != -1){
+        return elements[pairedPosFirst], elements[pairedPosSecond]
+    } else {
+        return -1, -1
+    }
+}
+
 func main() {
-	dat, err := ioutil.ReadFile("../input.txt")
-    check(err)
-    fmt.Print(string(dat))
+	lines, _ := readLines("../input.txt")
+
+    sort.Ints(lines)
+
+    num1, num2 := getSumPair(lines, 2020)
+
+    if (num1 != -1 && num2 != -1){
+        fmt.Print("\n\n")
+        fmt.Printf("Magic Numbers: %d + %d = %d.\n", num1, num2, num1+num2)
+        fmt.Printf("The solution is: %d x %d = %d.\n", num1, num2, num1*num2)
+    } else {
+        fmt.Println("No valid Pair found.")
+    }
+
+
 }
